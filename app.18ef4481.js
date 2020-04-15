c26e2653ab49904f52f7ad31996521100612ad4f
admin <admin@MacBook-Pro.local>
2020-04-16 06:39:42 +0700

    thu lai

--- app.18ef4481.js
+++
@@ -1,8 +0,0 @@
-parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"FRKU":[function(require,module,exports) {
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=void 0;var e=function(){return"\n        <header>\n            <h1>My Parcel app</h1>\n            <p> This is simple </p>\n        </header>\n    "},r=e;exports.default=r;
-},{}],"G5Xn":[function(require,module,exports) {
-
-},{}],"ffN9":[function(require,module,exports) {
-"use strict";var e=r(require("./components/Header"));function r(e){return e&&e.__esModule?e:{default:e}}require("./scss/app.scss");var n=function(){document.getElementById("header").innerHTML=(0,e.default)()};n();
-},{"./components/Header":"FRKU","./scss/app.scss":"G5Xn"}]},{},["ffN9"], null)
-//# sourceMappingURL=/app.18ef4481.js.map
